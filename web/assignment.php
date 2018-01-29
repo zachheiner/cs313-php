1b8dde02fdbb1b6b7b274d807e6dd518c7e22a4b
Zach Heiner <zachheiner@Zachs-MacBook-Pro.local>
2018-01-29 08:58:31 -0700

    assignment added

---
+++ web/assignment.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+	<head>
+    	<title>Zach Heiners Home Page</title>
+    	
+    	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+      <link rel="stylesheet" href="home.css">
+
+    	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  			<a class="navbar-brand" href="home.php">SHOELIT</a>
+  				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    				<span class="navbar-toggler-icon"></span>
+  				</button>
+
+  				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+    				<ul class="navbar-nav mr-auto">
+      					<li class="nav-item">
+        					<a class="nav-link" href="aboutus.php">ABOUT US</a>
+      					</li>
+                <li class="nav-item">
+                  <a class="nav-link active" href="assignment.php">ASSIGNMENTS</a>
+                </li>
+    				</ul>
+    				<form class="form-inline my-2 my-lg-0">
+      					<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      					<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    				</form>
+  				</div>
+		</nav>
+  </head>
+	<body>
+
+      <?php
+
+          echo "<br>";
+          echo "<br>";
+          echo "<br>";
+          echo "<h1><a class='link' href='assignment.php'>Shopping Cart</a></h1>";
+
+      ?>
+
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  </body>
+</html>
